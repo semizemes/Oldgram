cea1e9bf781b637f0ed8d384d6d343c24053fa68
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-20 22:28:16 +0500

    feat: implement dynamic post rendering and update HTML structure

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const posts = [
-  {
-    name: "Vincent van Gogh",
-    username: "vincey1853",
-    location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21,
-  },
-  {
-    name: "Gustave Courbet",
-    username: "gus1819",
-    location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4,
-  },
-  {
-    name: "Joseph Ducreux",
-    username: "jd1735",
-    location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment:
-      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152,
-  },
-];
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
 
+const container = document.getElementById("container");
 
+function createPosts(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    container.innerHTML += `
+        <article>
+                <img class="post-avatar" src="${arr[i].avatar}" >
+                <h1>${arr[i].name}</h1>
+                <p class="post-location">${arr[i].location}</p>
+                <img class="post-img" src="${arr[i].post}">
+                <img class="action-img icon-heart" src="images/icon-heart.png" >
+                <img class="action-img icon-comment" src="images/icon-comment.png" >
+                <img class="action-img icon-dm" src="images/icon-dm.png" >
+                <h2>${arr[i].likes} likes</h2>
+                
+                <p class="comment-text"><span class="comment-username">${arr[i].username} </span>${arr[i].comment}</p>
+            </article>`;
+  }
+}
+
+createPosts(posts);
